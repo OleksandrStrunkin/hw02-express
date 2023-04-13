a9c69a04a09103f418ac52cb96077940cc9175fe
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2023-04-13 17:55:24 +0300

    add.mongoose

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require('./app')
+const mongoose = require("mongoose")
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+const app = require('./app');
+const DB_HOST = "mongodb+srv://Olexander:9zOjazts5p82UiHv@cluster0.dxpnldy.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+.then(()=>app.listen(3000))
+.catch(error=>console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })

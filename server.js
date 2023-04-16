e437605ed1f7da27b3b49f84056c05e911d45529
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2023-04-16 14:23:45 +0300

    add Database connection successful

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const mongoose = require("mongoose")
 const app = require('./app');
 const DB_HOST = "mongodb+srv://Olexander:9zOjazts5p82UiHv@cluster0.dxpnldy.mongodb.net/contacts_reader?retryWrites=true&w=majority"
 mongoose.connect(DB_HOST)
-.then(()=>app.listen(3000))
-.catch(error=>console.log(error.message));
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    });
+  })
+  .catch(error => {
+    console.error("Database connection error:", error.message);
+    process.exit(1);
+  });
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000")
